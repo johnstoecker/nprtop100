f8fb5650ee6253c727e880ef659288a77f40b014
John Stoecker <johnstoecker@gmail.com>
2016-09-16 21:59:07 -0700

    add ratings for books

--- assets/main.js
+++ assets/main.js
@@ -5,25 +5,28 @@ angular.module('bookChallenge', [])
     $scope.bookData = [];
     $scope.columnNames = [];
     $scope.names = [];
-    $scope.extraCols = [];
+    $scope.comments = [];
+    $scope.ratings = [];
 
     Tabletop.init({
       key: "1Avp1EwBcBqS1kW1d9FSN0QA8Uj5J2InqXnMdjIYqlkY"
     , callback: function(data,tableTop){
       $scope.totals = data.pop();
-      // $scope.tableTop = tableTop;
       endingCols = tableTop.models.Sheet1.column_names.slice(4)
       for(var i = 0; i < endingCols.length; i++){
-        if(endingCols[i].indexOf(" says")==-1){
+        if(endingCols[i].indexOf(" says")==-1 && endingCols[i].indexOf(" rates")==-1){
           $scope.names.push(endingCols[i]);
-        } else {
-          $scope.extraCols.push(endingCols[i]);
-          console.log(data[0][endingCols[i]]);
+        // } else if(endingCols[i].indexOf(" rates")!==-1) {
+        //   $scope.ratings.push(endingCols[i]);
+        // }
+        // else {
+        //   $scope.comments.push(endingCols[i]);
+        //   console.log(data[0][endingCols[i]]);
         }
       }
       console.log($scope.names);
       console.log($scope.totals);
-
+      window.bookData = data;
       $scope.bookData = data;
       $scope.$apply();
     }
