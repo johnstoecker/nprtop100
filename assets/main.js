5da22a21e43b9b4be24bced9f3ae2d02c6bc8ae4
John Stoecker <johnstoecker@gmail.com>
2016-09-17 01:35:24 -0700

    sorting and ratings

--- assets/main.js
+++ assets/main.js
@@ -3,10 +3,32 @@ angular.module('bookChallenge', [])
     //Title, Author, cover, ebook, John Charles
     //store in localStorage to cache
     $scope.bookData = [];
+    $scope.sortAttributes = [null, null];
+    $scope.sortReverse = false;
+    $scope.sortReverseString = "up";
     $scope.columnNames = [];
     $scope.names = [];
     $scope.comments = [];
     $scope.ratings = [];
+
+    $scope.changeSort = function() {
+      console.log($scope.sortAttribute);
+      console.log($scope.sortReverse);
+      if($scope.sortAttribute == 'rating' && $scope.sortReverse) {
+        $scope.sortAttributes = ['!!rating', 'rating', null];
+      } else {
+        $scope.sortAttributes = [$scope.sortAttribute, null];
+      }
+    }
+    $scope.reverseSort = function() {
+      $scope.sortReverse = !$scope.sortReverse;
+      if($scope.sortReverse){
+        $scope.sortReverseString = "down";
+      } else{
+        $scope.sortReverseString = "up";
+      }
+      $scope.changeSort();
+    }
 
     Tabletop.init({
       key: "1Avp1EwBcBqS1kW1d9FSN0QA8Uj5J2InqXnMdjIYqlkY"
@@ -16,16 +38,35 @@ angular.module('bookChallenge', [])
       for(var i = 0; i < endingCols.length; i++){
         if(endingCols[i].indexOf(" says")==-1 && endingCols[i].indexOf(" rates")==-1){
           $scope.names.push(endingCols[i]);
-        // } else if(endingCols[i].indexOf(" rates")!==-1) {
-        //   $scope.ratings.push(endingCols[i]);
-        // }
-        // else {
-        //   $scope.comments.push(endingCols[i]);
-        //   console.log(data[0][endingCols[i]]);
+        }
+      }
+
+      for(var i=0; i < data.length; i++) {
+        data[i]["ratings"] = []
+        for(var key in data[i]){
+          if(key.indexOf(" rates")!==-1 && data[i][key]!==""){
+            data[i]["ratings"].push(data[i][key]);
+          }
+        }
+        var sum = 0;
+        for(var r in data[i]["ratings"]){
+          sum += parseInt(data[i]["ratings"][r]);
+        }
+        if(sum != 0) {
+          data[i]["rating"] = (sum/data[i]["ratings"].length).toFixed(0);
         }
       }
       console.log($scope.names);
       console.log($scope.totals);
+      $scope.sortBySelectors = [{ name: 'NPR Ranking', value: null}, {name: 'Rating', value: 'rating'}];
+      $scope.sortByOptions = ['index'];
+      for(var i = 0; i < $scope.names.length; i++){
+        $scope.sortBySelectors.push({
+          name: 'Unread by '+$scope.names[i],
+          value: $scope.names[i]
+        });
+      }
+      console.log($scope.sortBySelectors);
       window.bookData = data;
       $scope.bookData = data;
       $scope.$apply();
